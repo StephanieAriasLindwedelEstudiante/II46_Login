f2dbd3fd433cfb45c986629e546d7d593744ef7b
StephanieAriasLindwedelEstudiante <step2@STEPHANIE_ARIAS>
2025-08-01 20:27:24 -0600

    Mejoras en DatabaseHelper y registro de usuarios
    Se ha añadido un parámetro opcional `isStoredProcedure` en los métodos `ExecuteNonQuery` y `ExecuteQuery` de la clase `DatabaseHelper`, permitiendo la ejecución de procedimientos almacenados. Se ha implementado un nuevo método `LogErrorToFile` y se ha mejorado el manejo de errores en `LogError`.
    
    En `Registro.aspx`, se ha añadido un campo para el apellido y se ha corregido un `RequiredFieldValidator` para la contraseña. También se ha declarado el control `txtApellido` en `Registro.aspx.designer.vb`.
    
    Finalmente, en `Registro.aspx.vb`, se ha importado `System.Data.SqlClient` y se ha implementado la función `RegistrarUsuario` para insertar nuevos usuarios en la base de datos, junto con el evento `btnRegistrar_Click` para gestionar el registro y redirección del usuario.

--- Registro.aspx.vb
+++ Registro.aspx.vb
@@ -1,8 +1,42 @@
-﻿Public Class Registro
+﻿Imports System.Data.SqlClient
+
+Public Class Registro
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
+    Protected Function RegistrarUsuario(usuario As Usuario) As Boolean
+        Try
+            Dim helper As New DatabaseHelper()
+            Dim query As String = "INSERT INTO Usuarios (EMAIL, CONTRASENIA, NOMBRE, APELLIDOS) VALUES (@Email, @Password, @Nombre, @Apellido);"
+            Dim parametros As New List(Of SqlParameter) From {
+                New SqlParameter("@Email", usuario.Email),
+                New SqlParameter("@Password", usuario.Password),
+                New SqlParameter("@Nombre", usuario.Nombre),
+                New SqlParameter("@Apellido", usuario.Apellido)
+            }
 
+            Dim rowsAffected As Integer = helper.ExecuteNonQuery(query, parametros)
+            Return rowsAffected > 0
+        Catch ex As Exception
+            ' Manejar la excepción según sea necesario
+            Return False
+        End Try
+    End Function
+    Protected Sub btnRegistrar_Click(sender As Object, e As EventArgs)
+        Dim usuario As New Usuario() With {
+            .Email = txtEmail.Text,
+            .Password = txtPass.Text,
+            .Nombre = txtNombre.Text,
+            .Apellido = txtApellido.Text
+        }
+        If RegistrarUsuario(usuario) Then
+            ' Redirigir al login o a la página de inicio
+            Response.Redirect("Login.aspx")
+        Else
+            lblError.Text = "Error al registrar el usuario. Inténtalo de nuevo."
+            lblError.Visible = True
+        End If
+    End Sub
 End Class
